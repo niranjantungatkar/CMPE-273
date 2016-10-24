7fe09e102455130ec60f42c08cd48ed388c4ac22
Niranjan Tungatkar <niranjananil.tungatkar@sjsu.edu>
2016-10-23 17:10:01 -0700

    mongo utility file added

--- CMPE273-Lab01-eBay/app.js
+++ CMPE273-Lab01-eBay/app.js
@@ -21,15 +21,32 @@ var express = require('express')
   , bid = require('./routes/func.bid')
   , logger = require('./routes/util.logger')
   
+  
+var mongoSessionConnectURL = "mongodb://localhost:27017/sessions";
+var mongoStore = require("connect-mongo")(session);
+
 var app = express();
 
 //session properties
-app.use(session({
+/*app.use(session({
 	cookieName: 'session', 
 	secret: 'eBay_client_session',    
 	duration: 30 * 60 * 1000,    
 	activeDuration: 5 * 60 * 1000
-}));
+}));*/
+
+//session properties
+app.use(session({
+	//cookieName: 'session', 
+	secret: 'eBay_client_session',
+	resave : false,
+	saveUninitialized : false,
+	duration: 30 * 60 * 1000,    
+	activeDuration: 5 * 60 * 1000,
+	store: new mongoStore({  
+		url: mongoSessionConnectURL})
+	})
+);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -82,7 +99,12 @@ app.post('/bidcheckout', bid.checkout);
 
 app.post('/loguserclick',logger.logclientclicks)
 
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+mongo.connect(mongoSessionConnectURL, function(){  
+	console.log('Connected to mongo at: ' + mongoSessionConnectURL); 
+	http.createServer(app).listen(app.get('port'), function(){  
+		console.log('Express server listening on port ' + app.get('port'));  
+	});  
 });
+/*http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});*/
